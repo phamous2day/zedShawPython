a26edea60fe74976f6c8b33016376ea6d0bf4ee3
David Pham <phamous2day@users.noreply.github.com>
2016-09-15 01:00:55 -0400

    Update ex40.py
    When you instantiate/create a class, what you get is an object. Essentially, "instantiate" means to create an object from a class.

--- ex40.py
+++ ex40.py
@@ -1,9 +1,15 @@
-import mystuff
-mystuff.apple()
+class Song(object):
+    def __init__(self, lyrics):
+        self.lyrics=lyrics
 
-class MyStuff(object):
-    def ___init__(self):
-        self.tangerine = "And now a thousand years between"
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print line
 
-    def apple(self):
-        print "I am Classy Apples!"
+happy_bday = Song(["Happy birthday to you", "I don't want to get sued", "So I'll stop right here boo"])
+
+bulls_on_parade = Song(["They rally around the family", "With pockets full of shells"])
+
+happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
