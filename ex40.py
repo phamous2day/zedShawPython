7541f0371fe21191e9b2f9b7fde647d04b4873e6
David Pham <phamous2day@users.noreply.github.com>
2016-09-14 23:48:16 -0400

    Add files via upload

---
+++ ex40.py
@@ -0,0 +1,9 @@
+import mystuff
+mystuff.apple()
+
+class MyStuff(object):
+    def ___init__(self):
+        self.tangerine = "And now a thousand years between"
+
+    def apple(self):
+        print "I am Classy Apples!"
